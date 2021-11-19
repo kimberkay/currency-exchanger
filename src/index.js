1f7ae1911e377d21cd4f0ea44a8495f25d72cb2e
Kim Brannian <brannian@gmail.com>
2021-11-19 11:27:30 -0800

    debug code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import  ExchangeRateAPI from './exchangeAPI.js';
+import  ExchangeRateAPI from './ExchangeAPI.js';
 
 
 
@@ -14,17 +14,18 @@ function clearFields() {
 }
 
 $(document).ready(function() {
-  $('#exchangeButton').click(function() {
+  $('#exchangeButton').click(function(response) {
     let usDollars = $("#amount");
     let currency= $("#pickCurrency").val();
+    console.log(currency);
     clearFields();
     let promise = ExchangeRateAPI.getRate(currency);
-    promise.then(function(response) {
+    promise.then(function() {
       const body = JSON.parse(response);
       $('#showConversion').text(`$${usDollars} is ${body.conversion_rates.currency}`);
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}`);
-      console.log("made it!")
+      console.log("made it!");
     });
   });
 });
