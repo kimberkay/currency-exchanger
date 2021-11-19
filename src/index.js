19aa0483e675ea669c028daeb7c123d802e3497c
Kim Brannian <brannian@gmail.com>
2021-11-19 13:56:47 -0800

    add code

--- src/index.js
+++ src/index.js
@@ -12,11 +12,12 @@ $(document).ready(function() {
   $('#form-convert').submit(function(event) {
     event.preventDefault();
     console.log("here i am");
-    let usDollars = $("#amount");
+    let usDollars = $("#amount").val();
+    console.log(usDollars);
     let promise = ExchangeRateAPI.getRate();
     promise.then(function(response) {
       const result = JSON.parse(response);
-      $('#showConversion').text(`$ $("amount") is ${result.conversion_rates.AUD}`);
+      $('#showConversion').text(` is ${result.conversion_rates.AUD}`);
       console.log("made it!");
     }, function(error) {
       $('#showErrors').text(`There was an error processing your request: ${error}`);
